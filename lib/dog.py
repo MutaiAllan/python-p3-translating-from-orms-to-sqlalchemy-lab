3584c60dde8481c9b620a0532ad201c67390d60f
MutaiAllan <MutaiAllan>
2023-09-13 19:28:43 +0300

    Passed all tests

--- lib/dog.py
+++ lib/dog.py
@@ -13,13 +13,13 @@ def get_all(session):
     return session.query(Dog).all()
 
 def find_by_name(session, name):
-    return session.query(Dog).filter(Dog.name == name).all()
+    return session.query(Dog).filter(Dog.name.like(name)).first()
 
 def find_by_id(session, id):
-    return session.query(Dog).filter(Dog.name == id).all()
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).all()
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
     updateDog = session.query(Dog).filter(Dog.name == dog.name).first()
